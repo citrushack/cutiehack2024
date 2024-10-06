8bc56acbad6883e3aeec54a3832bcf652ce48764
Amir Shanti <amirvs9675@gmail.com>
2024-10-06 12:50:17 -0700

    added navigation file, sign in

--- src/components/live/Landing.jsx
+++ src/components/live/Landing.jsx
@@ -1,12 +1,20 @@
+"use client";
+
+import { signIn } from "next-auth/react";
+
 const Landing = () => {
   return (
-    <div className="flex justify-evenly bg-[#021334] p-6 font-semibold text-white">
+    <div className="flex cursor-pointer justify-evenly bg-[#021334] p-6 font-semibold text-white">
       <div className="p-2">HOME</div>
+
       <div className="p-2">SCHEDULE</div>
 
       <div className="p-2">ABOUT US</div>
 
-      <div className="border-white p-2 font-goodtimes shadow-custom-white">
+      <div
+        className="border-white p-2 font-goodtimes shadow-custom-white"
+        onClick={() => signIn("google")}
+      >
         LOGIN
       </div>
     </div>
---
+++ src/data/Navigator.js
@@ -0,0 +1,20 @@
+const items = [
+  {
+    name: "HOME",
+    link: "/",
+  },
+  {
+    name: "SCHEDULE",
+    link: "/user/schedule",
+  },
+  {
+    name: "ABOUT",
+    link: "/user/about",
+  },
+  {
+    name: "LOGIN",
+    link: "#login",
+  },
+];
+
+export default items;
