289c0e121bee06e8a4a2617635a1c9a23cd678ad
Taahh <thepheonixblader@gmail.com>
2024-10-07 21:08:22 -0700

    run prettier

--- src/components/live/Team.jsx
+++ src/components/live/Team.jsx
@@ -1,153 +1,205 @@
 import Image from "next/image";
-import TeamBorder from "@/public/assets/window.svg"
-import YellowStar from "@/public/assets/star.svg"
-import YellowDiamond from "@/public/assets/staryellow.svg"
-import WhiteDiamond from "@/public/assets/starwhite.svg"
-import { TEAMS as TeamMembers, COMMITTEES as Committees } from "@/data/live/Team"
+import TeamBorder from "@/public/assets/window.svg";
+import YellowStar from "@/public/assets/star.svg";
+import YellowDiamond from "@/public/assets/staryellow.svg";
+import WhiteDiamond from "@/public/assets/starwhite.svg";
+import {
+  TEAMS as TeamMembers,
+  COMMITTEES as Committees,
+} from "@/data/live/Team";
 import { Righteous } from "next/font/google";
 
 const FONT = Righteous({
   weight: "400",
-  subsets: ["latin"]
-})
+  subsets: ["latin"],
+});
 
 const Team = () => {
   return (
-    <div className={`flex flex-col items-center justify-center bg-gradient-to-b from-cutie-blue-300 to-cutie-blue-200 ${FONT.className} pb-16`}>
+    <div
+      className={`flex flex-col items-center justify-center bg-gradient-to-b from-cutie-blue-300 to-cutie-blue-200 ${FONT.className} pb-16`}
+    >
       <div className="mt-5 text-cutie-yellow md:text-5xl">Meet the Team</div>
-      <div className="mt-10 text-white flex flex-col items-center justify-center">
-        <p className="text-cutie-tan md:text-3xl mb-5">Directors</p>
-        <div className="w-2/5 relative left-48 -top-10">
+      <div className="mt-10 flex flex-col items-center justify-center text-white">
+        <p className="mb-5 text-cutie-tan md:text-3xl">Directors</p>
+        <div className="relative -top-10 left-48 w-2/5">
           <Image src={YellowStar} alt="Yellow Star" className="absolute w-10" />
-          <Image src={YellowDiamond} alt="Yellow Diamond" className="absolute left-12 -top-8 w-6" />
-          <Image src={WhiteDiamond} alt="White Diamond" className="absolute left-12 top-10 w-6" />
+          <Image
+            src={YellowDiamond}
+            alt="Yellow Diamond"
+            className="absolute -top-8 left-12 w-6"
+          />
+          <Image
+            src={WhiteDiamond}
+            alt="White Diamond"
+            className="absolute left-12 top-10 w-6"
+          />
         </div>
-        <div className="flex-row mt-3 gap-x-24">
-          {TeamMembers.filter(val => val.team == "Director").map((val, index) => {
-            return (
-              <div className="justify-center items-center flex flex-col">
-                <Image src={TeamBorder} key={index} alt="Team Member" className="mb-3" />
-                <p className="text-lg">{val.name}</p>
-                <p className="text-md">{val.team}</p>
-              </div>
-            )
-          })}
+        <div className="mt-3 flex-row gap-x-24">
+          {TeamMembers.filter((val) => val.team == "Director").map(
+            (val, index) => {
+              return (
+                <div className="flex flex-col items-center justify-center">
+                  <Image
+                    src={TeamBorder}
+                    key={index}
+                    alt="Team Member"
+                    className="mb-3"
+                  />
+                  <p className="text-lg">{val.name}</p>
+                  <p className="text-md">{val.team}</p>
+                </div>
+              );
+            },
+          )}
         </div>
-        <div className="w-2/5 relative -left-48 -top-8">
+        <div className="relative -left-48 -top-8 w-2/5">
           <Image src={YellowStar} alt="Yellow Star" className="absolute w-10" />
-          <Image src={YellowDiamond} alt="Yellow Diamond" className="absolute left-12 -top-8 w-6" />
-          <Image src={WhiteDiamond} alt="White Diamond" className="absolute left-12 top-10 w-6" />
+          <Image
+            src={YellowDiamond}
+            alt="Yellow Diamond"
+            className="absolute -top-8 left-12 w-6"
+          />
+          <Image
+            src={WhiteDiamond}
+            alt="White Diamond"
+            className="absolute left-12 top-10 w-6"
+          />
         </div>
       </div>
-      <div className="mt-20 text-white flex flex-col items-center justify-center">
-        <p className="text-cutie-tan md:text-3xl mb-5">Leads</p>
-        <div className="flex-row mt-3 flex-wrap w-7/12 gap-y-12 text-center justify-center">
-          {TeamMembers.filter(val => val.team == "Operations").map((val, index) => {
-            return (
-              <div className="justify-center items-center flex flex-col w-56">
-                <Image src={TeamBorder} key={index} alt="Team Member" className="mb-3" />
-                <p className="text-xl">{val.name}</p>
-                <p className="text-md">{val.team}</p>
-              </div>
-            )
-          })}
-          {TeamMembers.filter(val => val.team == "Marketing").map((val, index) => {
-            return (
-              <div className="justify-center items-center flex flex-col w-56 text-center">
-                <Image src={TeamBorder} key={index} alt="Team Member" className="mb-3" />
-                <p className="text-xl">{val.name}</p>
-                <p className="text-md">{val.team}</p>
-              </div>
-            )
-          })}
-          {TeamMembers.filter(val => val.team == "Sponsorship").map((val, index) => {
-            return (
-              <div className="justify-center items-center flex flex-col w-56 text-center">
-                <Image src={TeamBorder} key={index} alt="Team Member" className="mb-3" />
-                <p className="text-xl">{val.name}</p>
-                <p className="text-md">{val.team}</p>
-              </div>
-            )
-          })}
-          {TeamMembers.filter(val => val.team == "UI/UX").map((val, index) => {
-            return (
-              <div className="justify-center items-center flex flex-col w-56 text-center">
-                <Image src={TeamBorder} key={index} alt="Team Member" className="mb-3" />
-                <p className="text-xl">{val.name}</p>
-                <p className="text-md">{val.team}</p>
-              </div>
-            )
-          })}
+      <div className="mt-20 flex flex-col items-center justify-center text-white">
+        <p className="mb-5 text-cutie-tan md:text-3xl">Leads</p>
+        <div className="mt-3 w-7/12 flex-row flex-wrap justify-center gap-y-12 text-center">
+          {TeamMembers.filter((val) => val.team == "Operations").map(
+            (val, index) => {
+              return (
+                <div className="flex w-56 flex-col items-center justify-center">
+                  <Image
+                    src={TeamBorder}
+                    key={index}
+                    alt="Team Member"
+                    className="mb-3"
+                  />
+                  <p className="text-xl">{val.name}</p>
+                  <p className="text-md">{val.team}</p>
+                </div>
+              );
+            },
+          )}
+          {TeamMembers.filter((val) => val.team == "Marketing").map(
+            (val, index) => {
+              return (
+                <div className="flex w-56 flex-col items-center justify-center text-center">
+                  <Image
+                    src={TeamBorder}
+                    key={index}
+                    alt="Team Member"
+                    className="mb-3"
+                  />
+                  <p className="text-xl">{val.name}</p>
+                  <p className="text-md">{val.team}</p>
+                </div>
+              );
+            },
+          )}
+          {TeamMembers.filter((val) => val.team == "Sponsorship").map(
+            (val, index) => {
+              return (
+                <div className="flex w-56 flex-col items-center justify-center text-center">
+                  <Image
+                    src={TeamBorder}
+                    key={index}
+                    alt="Team Member"
+                    className="mb-3"
+                  />
+                  <p className="text-xl">{val.name}</p>
+                  <p className="text-md">{val.team}</p>
+                </div>
+              );
+            },
+          )}
+          {TeamMembers.filter((val) => val.team == "UI/UX").map(
+            (val, index) => {
+              return (
+                <div className="flex w-56 flex-col items-center justify-center text-center">
+                  <Image
+                    src={TeamBorder}
+                    key={index}
+                    alt="Team Member"
+                    className="mb-3"
+                  />
+                  <p className="text-xl">{val.name}</p>
+                  <p className="text-md">{val.team}</p>
+                </div>
+              );
+            },
+          )}
 
-          {TeamMembers.filter(val => val.team == "Software").map((val, index) => {
-            return (
-              <div className="justify-center items-center flex flex-col w-56 text-center">
-                <Image src={TeamBorder} key={index} alt="Team Member" className="mb-3" />
-                <p className="text-xl">{val.name}</p>
-                <p className="text-md">{val.team}</p>
-              </div>
-            )
-          })}
-
+          {TeamMembers.filter((val) => val.team == "Software").map(
+            (val, index) => {
+              return (
+                <div className="flex w-56 flex-col items-center justify-center text-center">
+                  <Image
+                    src={TeamBorder}
+                    key={index}
+                    alt="Team Member"
+                    className="mb-3"
+                  />
+                  <p className="text-xl">{val.name}</p>
+                  <p className="text-md">{val.team}</p>
+                </div>
+              );
+            },
+          )}
         </div>
       </div>
-      <div className="mt-20 text-white flex flex-col items-center justify-center">
-        <p className="text-cutie-tan md:text-3xl mb-5">Committees</p>
-        <div className="flex flex-row mt-3 flex-wrap w-full gap-x-16 text-center justify-center">
+      <div className="mt-20 flex flex-col items-center justify-center text-white">
+        <p className="mb-5 text-cutie-tan md:text-3xl">Committees</p>
+        <div className="mt-3 flex w-full flex-row flex-wrap justify-center gap-x-16 text-center">
           <div>
-            <div className="text-xl mb-2">Operations</div>
-            {Committees.filter(val => val.team == "Operations").map((val, index) => {
-              return (
-                <div className="">
-                  {val.name}
-                </div>
-              )
-            })}
+            <div className="mb-2 text-xl">Operations</div>
+            {Committees.filter((val) => val.team == "Operations").map(
+              (val, index) => {
+                return <div className="">{val.name}</div>;
+              },
+            )}
           </div>
           <div>
-            <div className="text-xl mb-2">Sponsorship</div>
-            {Committees.filter(val => val.team == "Sponsorship").map((val, index) => {
-              return (
-                <div className="">
-                  {val.name}
-                </div>
-              )
-            })}
+            <div className="mb-2 text-xl">Sponsorship</div>
+            {Committees.filter((val) => val.team == "Sponsorship").map(
+              (val, index) => {
+                return <div className="">{val.name}</div>;
+              },
+            )}
           </div>
           <div>
-            <div className="text-xl mb-2">Marketing</div>
-            {Committees.filter(val => val.team == "Marketing").map((val, index) => {
-              return (
-                <div className="">
-                  {val.name}
-                </div>
-              )
-            })}
+            <div className="mb-2 text-xl">Marketing</div>
+            {Committees.filter((val) => val.team == "Marketing").map(
+              (val, index) => {
+                return <div className="">{val.name}</div>;
+              },
+            )}
           </div>
           <div>
-            <div className="text-xl mb-2">Software</div>
-            {Committees.filter(val => val.team == "Software").map((val, index) => {
-              return (
-                <div className="">
-                  {val.name}
-                </div>
-              )
-            })}
+            <div className="mb-2 text-xl">Software</div>
+            {Committees.filter((val) => val.team == "Software").map(
+              (val, index) => {
+                return <div className="">{val.name}</div>;
+              },
+            )}
           </div>
           <div>
-            <div className="text-xl mb-2">UI/UX</div>
-            {Committees.filter(val => val.team == "UI/UX").map((val, index) => {
-              return (
-                <div className="">
-                  {val.name}
-                </div>
-              )
-            })}
+            <div className="mb-2 text-xl">UI/UX</div>
+            {Committees.filter((val) => val.team == "UI/UX").map(
+              (val, index) => {
+                return <div className="">{val.name}</div>;
+              },
+            )}
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 };
 
 export default Team;
--- src/data/live/Team.js
+++ src/data/live/Team.js
@@ -1,4 +1,4 @@
-import { StaticImport } from "next/dist/shared/lib/get-img-props"
+import { StaticImport } from "next/dist/shared/lib/get-img-props";
 
 // type Team = "Director" | "Operations" | "Software" | "Marketing" | "UI/UX"
 
@@ -10,199 +10,199 @@ import { StaticImport } from "next/dist/shared/lib/get-img-props"
 // }
 
 export const COMMITTEES = [
-    {
-        name: "First Last",
-        team: "Operations"
-    },
-    {
-        name: "First Last",
-        team: "Operations"
-    },
-    {
-        name: "First Last",
-        team: "Operations"
-    },
-    {
-        name: "First Last",
-        team: "Operations"
-    },
+  {
+    name: "First Last",
+    team: "Operations",
+  },
+  {
+    name: "First Last",
+    team: "Operations",
+  },
+  {
+    name: "First Last",
+    team: "Operations",
+  },
+  {
+    name: "First Last",
+    team: "Operations",
+  },
 
-    {
-        name: "First Last",
-        team: "Sponsorship"
-    },
-    {
-        name: "First Last",
-        team: "Sponsorship"
-    },
-    {
-        name: "First Last",
-        team: "Sponsorship"
-    },
-    {
-        name: "First Last",
-        team: "Sponsorship"
-    },
-    {
-        name: "First Last",
-        team: "Sponsorship"
-    },
+  {
+    name: "First Last",
+    team: "Sponsorship",
+  },
+  {
+    name: "First Last",
+    team: "Sponsorship",
+  },
+  {
+    name: "First Last",
+    team: "Sponsorship",
+  },
+  {
+    name: "First Last",
+    team: "Sponsorship",
+  },
+  {
+    name: "First Last",
+    team: "Sponsorship",
+  },
 
-    {
-        name: "First Last",
-        team: "Marketing"
-    },
-    {
-        name: "First Last",
-        team: "Marketing"
-    },
-    {
-        name: "First Last",
-        team: "Marketing"
-    },
+  {
+    name: "First Last",
+    team: "Marketing",
+  },
+  {
+    name: "First Last",
+    team: "Marketing",
+  },
+  {
+    name: "First Last",
+    team: "Marketing",
+  },
 
-    {
-        name: "First Last",
-        team: "Software"
-    },
-    {
-        name: "First Last",
-        team: "Software"
-    },
-    {
-        name: "First Last",
-        team: "Software"
-    },
-    {
-        name: "First Last",
-        team: "Software"
-    },
-    {
-        name: "First Last",
-        team: "Software"
-    },
-    {
-        name: "First Last",
-        team: "Software"
-    },
+  {
+    name: "First Last",
+    team: "Software",
+  },
+  {
+    name: "First Last",
+    team: "Software",
+  },
+  {
+    name: "First Last",
+    team: "Software",
+  },
+  {
+    name: "First Last",
+    team: "Software",
+  },
+  {
+    name: "First Last",
+    team: "Software",
+  },
+  {
+    name: "First Last",
+    team: "Software",
+  },
 
-    {
-        name: "First Last",
-        team: "UI/UX"
-    },
-    {
-        name: "First Last",
-        team: "UI/UX"
-    },
+  {
+    name: "First Last",
+    team: "UI/UX",
+  },
+  {
+    name: "First Last",
+    team: "UI/UX",
+  },
 ];
 
 export const TEAMS = [
-    // DIRECTORS
-    {
-        name: "Minnoli Nori",
-        link: "/",
-        image: "",
-        team: "Director"
-    },
-    {
-        name: "Jon Darius",
-        link: "/",
-        image: "",
-        team: "Director"
-    },
-    // OPERATIONS
-    {
-        name: "Allison Pham",
-        link: "/",
-        image: "",
-        team: "Operations"
-    },
-    {
-        name: "Huong Le",
-        link: "/",
-        image: "",
-        team: "Operations"
-    },
-    {
-        name: "Mahiyat Bhuiyan",
-        link: "/",
-        image: "",
-        team: "Operations"
-    },
-    {
-        name: "Karan Dhawan",
-        link: "/",
-        image: "",
-        team: "Operations"
-    },
-    // MARKETING
-    {
-        name: "Chandrahass Mariserla",
-        link: "/",
-        image: "",
-        team: "Marketing"
-    },
-    {
-        name: "Hannah Hwang",
-        link: "/",
-        image: "",
-        team: "Marketing"
-    },
-    // SPONSORSHIP
-    {
-        name: "Rhea Verma",
-        link: "/",
-        image: "",
-        team: "Sponsorship"
-    },
-    {
-        name: "Cris LastName",
-        link: "/",
-        image: "",
-        team: "Sponsorship"
-    },
-    // UI / UX
-    {
-        name: "Ananya Dharanikota",
-        link: "/",
-        image: "",
-        team: "UI/UX"
-    },
-    {
-        name: "Vishra Thakkar",
-        link: "/",
-        image: "",
-        team: "UI/UX"
-    },
-    {
-        name: "Dylan Vuong",
-        link: "/",
-        image: "",
-        team: "UI/UX"
-    },
-    // SOFTWARE
-    {
-        name: "Jonathan Trujillo",
-        link: "/",
-        image: "",
-        team: "Software"
-    },
-    {
-        name: "Taaha Sayed",
-        link: "/",
-        image: "",
-        team: "Software"
-    },
-    {
-        name: "Amir Shanti",
-        link: "/",
-        image: "",
-        team: "Software"
-    },
-    {
-        name: "Holland Pleskac",
-        link: "/",
-        image: "",
-        team: "Software"
-    }
+  // DIRECTORS
+  {
+    name: "Minnoli Nori",
+    link: "/",
+    image: "",
+    team: "Director",
+  },
+  {
+    name: "Jon Darius",
+    link: "/",
+    image: "",
+    team: "Director",
+  },
+  // OPERATIONS
+  {
+    name: "Allison Pham",
+    link: "/",
+    image: "",
+    team: "Operations",
+  },
+  {
+    name: "Huong Le",
+    link: "/",
+    image: "",
+    team: "Operations",
+  },
+  {
+    name: "Mahiyat Bhuiyan",
+    link: "/",
+    image: "",
+    team: "Operations",
+  },
+  {
+    name: "Karan Dhawan",
+    link: "/",
+    image: "",
+    team: "Operations",
+  },
+  // MARKETING
+  {
+    name: "Chandrahass Mariserla",
+    link: "/",
+    image: "",
+    team: "Marketing",
+  },
+  {
+    name: "Hannah Hwang",
+    link: "/",
+    image: "",
+    team: "Marketing",
+  },
+  // SPONSORSHIP
+  {
+    name: "Rhea Verma",
+    link: "/",
+    image: "",
+    team: "Sponsorship",
+  },
+  {
+    name: "Cris LastName",
+    link: "/",
+    image: "",
+    team: "Sponsorship",
+  },
+  // UI / UX
+  {
+    name: "Ananya Dharanikota",
+    link: "/",
+    image: "",
+    team: "UI/UX",
+  },
+  {
+    name: "Vishra Thakkar",
+    link: "/",
+    image: "",
+    team: "UI/UX",
+  },
+  {
+    name: "Dylan Vuong",
+    link: "/",
+    image: "",
+    team: "UI/UX",
+  },
+  // SOFTWARE
+  {
+    name: "Jonathan Trujillo",
+    link: "/",
+    image: "",
+    team: "Software",
+  },
+  {
+    name: "Taaha Sayed",
+    link: "/",
+    image: "",
+    team: "Software",
+  },
+  {
+    name: "Amir Shanti",
+    link: "/",
+    image: "",
+    team: "Software",
+  },
+  {
+    name: "Holland Pleskac",
+    link: "/",
+    image: "",
+    team: "Software",
+  },
 ];
