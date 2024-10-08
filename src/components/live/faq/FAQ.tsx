f8eaca62cdbc3b619b92de920dbf2473e4460fba
sunbrandon <sunabrandon@gmail.com>
2024-10-08 13:12:47 -0700

    Styled FAQ component

--- src/components/live/faq/FAQ.tsx
+++ src/components/live/faq/FAQ.tsx
@@ -3,13 +3,22 @@ import Item from "./Item";
 
 const FAQ = () => {
   return (
-    <div className="flex h-full w-full flex-col items-center">
-      <div className="flex w-8/12 flex-col overflow-hidden">
-        {QUESTIONS.map((faq, index) => (
-          <Item key={index} title={faq.question}>
-            {faq.answer}
-          </Item>
-        ))}
+    <div className="flex h-full w-full flex-col items-center font-righteous text-white">
+      <div className="w-5/12 rounded-3xl border-8 border-[#A22282] bg-[#61114D] p-6 shadow-lg">
+        <div className="flex w-full flex-col space-y-2 overflow-hidden">
+          {QUESTIONS.map((faq, index) => (
+            <div
+              key={index}
+              className={`${
+                index !== QUESTIONS.length - 1
+                  ? "border-b border-white pb-1"
+                  : ""
+              }`}
+            >
+              <Item title={faq.question}>{faq.answer}</Item>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
