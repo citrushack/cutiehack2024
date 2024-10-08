2591c224536b0a88da948252f25eb24d7b0f7e52
stormyy00 <jonathan.trujillo0432@gmail.com>
2024-10-07 18:07:23 -0700

    rm underline

--- src/components/live/Apply.jsx
+++ src/components/live/Apply.jsx
@@ -5,7 +5,7 @@ const Apply = ({ text, link }) => {
   return (
     <Link
       href={link}
-      className="flex w-full items-center p-1 px-4 font-righteous text-base font-bold text-cutie-magenta underline hover:opacity-75 md:text-3xl"
+      className="flex w-full items-center p-1 px-4 font-righteous text-base font-bold text-cutie-magenta hover:opacity-75 md:text-3xl"
     >
       {text}
     </Link>
