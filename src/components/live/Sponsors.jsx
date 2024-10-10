4e654e566417fbfe6c3739fee61ab130d008cc5f
Holland Pleskac <hollandpleskac@gmail.com>
2024-10-09 23:27:49 -0700

    npm run format changes

--- src/components/live/Sponsors.jsx
+++ src/components/live/Sponsors.jsx
@@ -1,50 +1,75 @@
+import {
+  CoolerMaster,
+  SponsorFlag,
+  SponsorSmall,
+  RingRight,
+  RingLeft,
+} from "@/public/assets/sponsors";
 
-import { CoolerMaster, SponsorFlag, SponsorSmall, RingRight, RingLeft } from '@/public/assets/sponsors';
-
-import Image from 'next/image';
+import Image from "next/image";
 const Sponsors = () => {
-  return <div className="relative flex flex-col items-center justify-center px-[50px] sm:px-[107px] pb-[191px] bg-cutie-blue-300">
+  return (
+    <div className="relative flex flex-col items-center justify-center bg-cutie-blue-300 px-[50px] pb-[191px] sm:px-[107px]">
+      <div className="absolute left-0 top-0 z-0">
+        <Image src={RingLeft} alt="Left Ring" className="h-full w-full" />
+      </div>
 
-    <div className='absolute left-0 top-0 z-0' >
-      <Image src={RingLeft} alt="Left Ring" className=" h-full w-full" />
+      <div className="absolute right-0 top-[600px] z-0 lg:top-[450px] 2xl:top-[150px]">
+        <Image src={RingRight} alt="Right Ring" className="h-full w-full" />
+      </div>
+
+      <div className="z-10 mt-[156px] flex w-full flex-wrap justify-center gap-x-[31px] gap-y-[20px]">
+        <div className="w-[417px]">
+          <Image
+            src={CoolerMaster}
+            alt="Cooler Master"
+            className="h-auto max-h-[466px] w-full object-contain"
+          />
+        </div>
+        <div className="w-[417px]">
+          <Image
+            src={CoolerMaster}
+            alt="Cooler Master"
+            className="h-auto max-h-[466px] w-full object-contain"
+          />
+        </div>
+        <div className="w-[417px]">
+          <Image
+            src={CoolerMaster}
+            alt="Cooler Master"
+            className="h-auto max-h-[466px] w-full object-contain"
+          />
+        </div>
+      </div>
+
+      <div className="z-10 mt-[77px] flex w-full flex-wrap justify-center gap-x-[173px] gap-y-[20px]">
+        <div className="w-[306px]">
+          <Image
+            src={SponsorFlag}
+            alt="Small Sponsor"
+            className="h-auto max-h-[347px] w-full object-contain"
+          />
+        </div>
+        <div className="w-[306px]">
+          <Image
+            src={SponsorFlag}
+            alt="Small Sponsor"
+            className="h-auto max-h-[347px] w-full object-contain"
+          />
+        </div>
+      </div>
+
+      <div className="z-10 mt-[23px] flex w-full flex-wrap justify-center gap-x-[173px] gap-y-[20px]">
+        <div className="w-[190px]">
+          <Image
+            src={SponsorSmall}
+            alt="XS Sponsor"
+            className="h-auto max-h-[255px] w-full object-contain"
+          />
+        </div>
+      </div>
     </div>
-
-    <div className='absolute right-0 top-[600px] lg:top-[450px] 2xl:top-[150px] z-0' >
-      <Image src={RingRight} alt="Right Ring" className=" h-full w-full" />
-    </div>
-
-
-    <div className="flex flex-wrap justify-center gap-x-[31px] gap-y-[20px] mt-[156px] z-10 w-full">
-      <div className="w-[417px]">
-        <Image src={CoolerMaster} alt="Cooler Master" className="w-full h-auto max-h-[466px] object-contain" />
-      </div>
-      <div className="w-[417px]">
-        <Image src={CoolerMaster} alt="Cooler Master" className="w-full h-auto max-h-[466px] object-contain" />
-      </div>
-      <div className="w-[417px]">
-        <Image src={CoolerMaster} alt="Cooler Master" className="w-full h-auto max-h-[466px] object-contain" />
-      </div>
-    </div>
-
-    
-
-    <div className="flex flex-wrap justify-center gap-x-[173px] gap-y-[20px] mt-[77px] z-10 w-full">
-      <div className="w-[306px]">
-        <Image src={SponsorFlag} alt="Small Sponsor" className="w-full h-auto max-h-[347px] object-contain" />
-      </div>
-      <div className="w-[306px]">
-        <Image src={SponsorFlag} alt="Small Sponsor" className="w-full h-auto max-h-[347px] object-contain" />
-      </div>
-    </div>
-
-    <div className="flex flex-wrap justify-center gap-x-[173px] gap-y-[20px] mt-[23px] z-10 w-full">
-      <div className="w-[190px] ">
-        <Image src={SponsorSmall} alt="XS Sponsor" className="w-full h-auto max-h-[255px] object-contain" />
-      </div>
-    </div>
-
-
-  </div>;
+  );
 };
 
 export default Sponsors;
