d3f1d71d80af684057f483f14225960943293e8f
stormyy00 <jonathan.trujillo0432@gmail.com>
2024-10-22 17:47:07 -0700

    add ons

--- src/components/live/faq/FAQ.tsx
+++ src/components/live/faq/FAQ.tsx
@@ -5,10 +5,17 @@ import Item from "./Item";
 
 const FAQ = () => {
   return (
-    <div className="flex h-full w-full flex-col items-center bg-cutie-blue-300 py-10 font-righteous text-white">
+    <div
+      id="faq"
+      className="flex h-full w-full flex-col items-center bg-cutie-blue-300 py-10 font-righteous text-white"
+    >
       <div className="flex items-center justify-center gap-2 py-4 align-middle text-7xl">
         <h1>F</h1>
-        <Image src={UFO} alt="UFO" className="size-1/3" />
+        <Image
+          src={UFO}
+          alt="UFO"
+          className="-mt-5 size-1/3 animate-jiggle-12s"
+        />
         <h1>Q</h1>
       </div>
       <div className="w-11/12 rounded-3xl border-8 border-[#A22282] bg-[#61114D] p-6 shadow-lg md:w-5/12">
