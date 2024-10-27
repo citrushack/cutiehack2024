a3a68633c0d4a85a2a6850b5c6788eb7152d6caf
shahdivyank <divyank.shah.2016@gmail.com>
2024-10-27 02:03:22 -0700

    pog

--- src/components/form/form/index.jsx
+++ src/components/form/form/index.jsx
@@ -38,7 +38,7 @@ const Form = ({
       </div>
       <div className="flex w-10/12 flex-col items-center pb-12 pt-5 md:w-1/2 xl:w-1/3">
         <Image src={LOGO} className="m-4 w-1/4" alt="Logo" />
-        <p className="m-0 w-full rounded-t-xl bg-hackathon-green-300 px-4 py-2 text-xl font-semibold">
+        <p className="m-0 w-full rounded-t-xl bg-hackathon-green-300 px-4 py-4 text-xl font-semibold">
           {header}
         </p>
         <div className="rounded-b-xl bg-white p-8">
