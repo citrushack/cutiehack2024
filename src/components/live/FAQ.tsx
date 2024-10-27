5fea6e7e08e47e34f6272b9116ad5be8151c621e
shahdivyank <divyank.shah.2016@gmail.com>
2024-10-27 00:26:35 -0700

    fix live

---
+++ src/components/live/FAQ.tsx
@@ -0,0 +1,26 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { QUESTIONS } from "@/data/FAQ";
+
+const FAQ = () => {
+  return (
+    <div className="flex h-full w-full flex-col items-center">
+      <div className="w-10/12">
+        <Accordion type="single" collapsible className="w-full">
+          {QUESTIONS.map(({ question, answer }, index) => (
+            <AccordionItem value={question} key={index}>
+              <AccordionTrigger>{question}</AccordionTrigger>
+              <AccordionContent>{answer}</AccordionContent>
+            </AccordionItem>
+          ))}
+        </Accordion>
+      </div>
+    </div>
+  );
+};
+
+export default FAQ;
--- src/components/live/Live.tsx
+++ src/components/live/Live.tsx
@@ -1,16 +1,17 @@
 import Landing from "./Landing";
 import About from "./About";
-import Schedule from "./schedule/Schedule";
+import Schedule from "./schedule";
 import Tracks from "./Tracks";
 import Sponsors from "./Sponsors";
 import Team from "./Team";
 import Committees from "./Committees";
 import Judges from "./Judges";
-import FAQ from "./faq/FAQ";
+import FAQ from "./FAQ";
 import Footer from "./Footer";
+
 const Live = () => {
   return (
-    <div>
+    <>
       <Landing />
       <About />
       <Tracks />
@@ -21,7 +22,7 @@ const Live = () => {
       <Judges />
       <FAQ />
       <Footer />
-    </div>
+    </>
   );
 };
 
