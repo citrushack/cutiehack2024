a746f86e69aea03ffc7ed9d0b78e1b9a55e649a7
stormyy00 <jonathan.trujillo0432@gmail.com>
2024-11-05 00:10:56 -0800

    z index

--- src/components/live/Landing.tsx
+++ src/components/live/Landing.tsx
@@ -25,7 +25,7 @@ const Landing = () => {
           <div className="block font-righteous text-lg font-medium md:hidden">
             Apply to
           </div>
-          <div className="mb-4 mt-1 flex items-center justify-center whitespace-normal font-righteous text-base font-medium md:whitespace-nowrap md:text-3xl">
+          <div className="mb-4 mt-1 flex items-center justify-center whitespace-normal font-righteous text-base font-medium md:whitespace-nowrap md:text-3xl z-20">
             <p className="hidden md:block">Apply to</p>
             <Apply text={"REGISTER"} link={"/form/participant"} />
             <p>or</p>
